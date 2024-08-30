930cb78f6c2da4792b6622e376f55e9af130bb5f
tarun1sisodia <coccoder999@gmail.com>
2024-08-31 02:35:06 +0530

    Update the AppBar

--- big_brains/lib/day1.dart
+++ big_brains/lib/day1.dart
@@ -13,12 +13,19 @@ class Day1 extends StatelessWidget
           title: Text("Android Studio"),
           centerTitle:true,
           backgroundColor: Colors.amber,
+          leading: Icon(Icons.menu,color: const Color.fromARGB(255, 136, 26, 200)),
+          elevation: 0,
+          actions: [
+            Icon(Icons.search_rounded,color: Color.fromARGB(255, 136, 26, 200),),
+            Padding(padding: EdgeInsets.only(left:10)),
+            Icon(Icons.file_copy_outlined,color:Color.fromARGB(255,136,26,200)),
+            Padding(padding: EdgeInsets.only(left: 5),)
+          ],
         ),
         body: Container(
           child: Center(child: Text("Hello Studio")),
         ),
       ),
     );
-
   }
 }
