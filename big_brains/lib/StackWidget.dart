322eb91f3e8bfbab4bd8ebce368c8e4f51c7008f
tarun1sisodia <coccoder999@gmail.com>
2024-08-31 11:25:25 +0530

    Stack Widget

---
+++ big_brains/lib/StackWidget.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+class StackWidget extends StatelessWidget {
+const StackWidget({ super.key });
+
+  @override
+  Widget build(BuildContext context){
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Stack Widget",style:TextStyle(color: Colors.amber)),
+        backgroundColor: Colors.black,
+      ),
+      body: Stack(
+        children: [
+          Center(
+            child: Container(
+              height: 20,
+              width:150,
+              color:Colors.green,
+            ),
+          ),
+          Center(child: Text("Center Container")),
+          Container(
+              height: 30,
+              width:120,
+              color:Colors.green,
+            ),
+          Text("Default Container")
+        ],
+      ),
+    );
+  }
+}
