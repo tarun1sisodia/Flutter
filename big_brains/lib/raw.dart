7fd79400ab72db24f3d6baa5ad9344cc1f1f787d
tarun1sisodia <coccoder999@gmail.com>
2024-08-31 11:04:03 +0530

    Row Widget

---
+++ big_brains/lib/raw.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+
+class Raw extends StatelessWidget 
+{
+  const Raw({ super.key });
+
+  @override
+  Widget build(BuildContext context)
+  {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Row Widget',style: TextStyle(color: Colors.white),),
+        centerTitle: true,
+        backgroundColor: Colors.black,
+      ),
+      body: Row(
+        children: [
+          Text('Row Wise',style: TextStyle(fontSize: 15,color: Colors.blue),),
+          Container(
+            height: 50,
+            width: 100,
+            color: Colors.amber,
+          ),
+          Container(
+            height: 60,
+            width: 120,
+            color:Colors.amber[300]
+          ),
+          Container(
+            height: 50,
+            width: 100,
+            color: Colors.amber,
+          ),
+        ],
+      ),
+    );
+  }
+}
