fc7640203fa774f436b8823c1e38e558aefbfa03
tarun1sisodia <coccoder999@gmail.com>
2024-08-31 11:25:43 +0530

    Configure the Home

--- big_brains/lib/main.dart
+++ big_brains/lib/main.dart
@@ -7,6 +7,7 @@ import 'material.dart';
 import 'day0.dart';
 import 'column.dart';
 import 'raw.dart';
+import 'StackWidget.dart';
 
 void main() {
   runApp(const MainApp());
@@ -18,7 +19,7 @@ class MainApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      home: Raw(),
+      home: StackWidget(),
       debugShowCheckedModeBanner: false,
     );
   }
