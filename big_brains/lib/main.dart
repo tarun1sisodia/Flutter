0de6c9cc8a7a9037c55f90334162340e96056a0a
tarun1sisodia <coccoder999@gmail.com>
2024-08-31 10:05:48 +0530

    Remove Unused Import lib

--- big_brains/lib/main.dart
+++ big_brains/lib/main.dart
@@ -1,10 +1,8 @@
 import 'package:big_brains/Safeare.dart';
-import 'package:big_brains/material.dart';
 import 'package:flutter/material.dart';
 // import 'day1.dart';
 // import 'day2.dart';
 // import 'material.dart';
-import 'Safeare.dart';
 
 void main() {
   runApp(const MainApp());
