6010b4605de38bf7c0562699ed18b010c5499c4a
tarun1sisodia <coccoder999@gmail.com>
2024-08-31 23:11:08 +0530

    Design a basic Button of Elevated

---
+++ big_brains/lib/btn.dart
@@ -0,0 +1,64 @@
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+class Btn extends StatefulWidget 
+{
+  const Btn({ super.key });
+
+  @override
+  _BtnState createState() => _BtnState();
+}
+
+class _BtnState extends State<Btn> 
+/// Increments the count state variable.
+{
+  int count =0;
+
+  void increament()
+  {
+    setState(() {
+      count++;
+    });
+  }
+
+  void reset()
+  {
+    setState(() {
+      if(count == 0 || count < 1)
+      {
+        count--;
+      }
+      else if(count > 0 )
+      {
+        count = 0;
+      }
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) 
+  {
+    return Scaffold(
+      appBar: AppBar(title: Text("Deadpool X Wolvrine",style: TextStyle(color: Colors.yellow),),
+      centerTitle: true,
+      backgroundColor: Colors.red,
+      leading: FaIcon(FontAwesomeIcons.gun,color: Colors.white,),
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Center(child: const Text("Buy Ticket",style: TextStyle(fontSize: 30,color: Color.fromARGB(255, 206, 196, 46),fontWeight:FontWeight.bold,backgroundColor: Color.fromARGB(255, 255, 0, 136)),),),
+          SizedBox(height: 50,),
+          Text(count.toString(),style:TextStyle(fontSize:20,fontWeight: FontWeight.bold)),
+          Row(mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              ElevatedButton(onPressed: increament, child: const Text("Tap")),
+              
+              ElevatedButton(onPressed: reset, child: const Text("Reset"))
+          ],),
+          
+        ],
+      ),
+    );
+  }
+}
