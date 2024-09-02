3d2abd655ac2270ebc77af531f0fcbc825a9b831
Tarun sisodia <freecuetpg@gmail.com>
2024-09-02 20:11:17 +0530

    No Change

--- big_brains/lib/inputbox.dart
+++ big_brains/lib/inputbox.dart
@@ -8,67 +8,117 @@ class Inputbox extends StatefulWidget {
   State<Inputbox> createState() => _InputboxState();
 }
 
-class _InputboxState extends State<Inputbox> {
+class _InputboxState extends State<Inputbox> 
+{
   TextEditingController _controller = TextEditingController();
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context) 
+  {
+    //Returning to Widget to Create UI
     return Scaffold(
+      //Background for Whole body.
         backgroundColor: const Color.fromARGB(255, 255, 255, 255),
+
+        //APP BAR
         appBar: AppBar(
+
           elevation: 0,
           centerTitle: true,
-          leading: Icon(FontAwesomeIcons.youtube,
-              color: const Color.fromARGB(255, 255, 0, 0)),
-          title: Text(
-            "Youtube Login Page",
-            style: TextStyle(
-                fontWeight: FontWeight.bold,
-                color: const Color.fromARGB(255, 0, 0, 0)),
+
+          leading: Icon(
+
+            FontAwesomeIcons.youtube,
+            color: const Color.fromARGB(255, 255, 0, 0)
+
           ),
+          
+          title: Text(
+
+            "Youtube Login Page",
+
+            style: TextStyle(
+
+                fontWeight: FontWeight.bold,
+                color: const Color.fromARGB(255, 0, 0, 0)
+
+            ),
+          ),
+
           backgroundColor: Colors.amber,
+
         ),
+
         body: Column(
+
           mainAxisAlignment: MainAxisAlignment.center,
+
           children: [
-            Text("Welcome back!",style: TextStyle(fontSize: 20),),
+
+            Text(
+              "Welcome back!",
+              style: TextStyle(fontSize: 20),
+            ),
+
             Padding(
+
                 padding: const EdgeInsets.all(20.0),
+
                 child: TextFormField(
                   controller: _controller,
+
                   decoration: InputDecoration(
+                    
                       hintText: "Email",
+
                       hintStyle: TextStyle(
+
                         color: const Color.fromARGB(255, 0, 0, 0),
                         fontFamily: 'Open Sans',
+
                       ),
-                      
+
                       //it creates outline of whole textformfield.
                       border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(30)
+                          borderRadius: BorderRadius.circular(30)
                       )
-                      ),
-                )),
-                Padding(
-                  padding: const EdgeInsets.all(20.0),
-                  child: TextFormField(
-                    obscureText: true,
-                    decoration: InputDecoration(
+                  ),
+                )
+            ),
+            
+            Padding(
+
+                padding: const EdgeInsets.all(20.0),
+
+                child: TextFormField(
+
+                  obscureText: true,
+
+                  decoration: InputDecoration(
+
                       hintText: "Password",
+
                       hintStyle: TextStyle(
-                        color: const Color.fromARGB(255, 3, 0, 5),
-                        fontFamily: 'Open Sans'
-                      ),
+
+                          color: const Color.fromARGB(255, 3, 0, 5),
+                          fontFamily: 'Open Sans'),
+
                       alignLabelWithHint: true,
+
                       border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(30)
+
+                          borderRadius: BorderRadius.circular(30)
                       )
-                    ),
-                  )
-                ),
+                  ),
+                )
+            ),
             IconButton(
+
               onPressed: () {},
-              icon: Icon(FontAwesomeIcons.satelliteDish, color: Colors.amber,size: 25),
+
+              icon: Icon(FontAwesomeIcons.satelliteDish,
+                  color: Colors.amber, size: 25),
+
             ),
           ],
         ));
