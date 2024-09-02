397e6aac6cc58ab8fec2d3032765bc057c67877c
Tarun sisodia <freecuetpg@gmail.com>
2024-09-02 13:30:20 +0530

    Image Widget in App Locally

---
+++ big_brains/lib/ImageDay9.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class ImageDay9 extends StatelessWidget 
+{
+const ImageDay9({ super.key });
+
+  @override
+  Widget build(BuildContext context)
+  {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Import Image From File/Url"),
+        centerTitle: true,
+        backgroundColor: Colors.amber
+      ),
+      body: Center(
+        child: Container(
+          child: Image.asset("assets/Container.jpeg"),
+        )
+      )
+    );
+
+  }
+}
